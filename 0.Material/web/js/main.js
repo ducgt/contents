5ad610250cba9665ff0f31b71df1c827f3849738
qus0in <qus0in@gmail.com>
2019-12-20 18:38:51 +0900

    191220 Web 초안 (disqus 추가, code refactoring)

--- 0.Material/web/js/main.js
+++ 0.Material/web/js/main.js
@@ -12,30 +12,30 @@ function hide(card_id) {
 }
 
 const friends = [
-    {name: "김자바", img:"/img/face.png", group: "직장", detail: "김자바입니다!"},
-    {name: "이파선", img:"/img/face.png", group: "직장", detail: "이파선입니다!"},
-    {name: "최머신", img:"/img/face.png", group: "직장", detail: "최머신입니다!"},
-    {name: "박철수", img:"/img/face.png", group: "학교", detail: "박철수입니다!"},
-    {name: "정민수", img:"/img/face.png", group: "학교", detail: "정민수입니다!"},
-    {name: "안희영", img:"/img/face.png", group: "학교", detail: "안희영입니다!"}
+    {name: "김자바", img:"face.png", group: "직장", detail: "김자바입니다!"},
+    {name: "박철수", img:"face.png", group: "학교", detail: "박철수입니다!"},
+    {name: "이파선", img:"face.png", group: "직장", detail: "이파선입니다!"},
+    {name: "정민수", img:"face.png", group: "학교", detail: "정민수입니다!"},
+    {name: "최머신", img:"face.png", group: "직장", detail: "최머신입니다!"},
+    {name: "안희영", img:"face.png", group: "학교", detail: "안희영입니다!"}
 ]
 
 function friendsAll() {
     document.getElementById("myfriends").innerHTML = ""
     friends.forEach(v => {
         console.log(v)
-        document.getElementById("myfriends").innerHTML += '<div class="bfimg"><img src="' + v.img + '"></div>';
+        document.getElementById("myfriends").innerHTML += '<img style="width: 9%; padding: 1rem" src="img/' + v.img + '">';
         document.getElementById("myfriends").innerHTML
-        += '<div class="bfname"><p><b>[' + v.name + "]</b> " + v.detail + ' (' + v.group  + ')</p></div>'
+        += '<div class="bfname"><b>[' + v.name + "]</b> " + v.detail + ' (' + v.group  + ')</div><br>'
     });
 }
 function friendsGroup(group) {
     document.getElementById("myfriends").innerHTML = ""
     friends.filter(v => v.group == group).forEach(v => {
         console.log(v)
-        document.getElementById("myfriends").innerHTML += '<div class="bfimg"><img src="' + v.img + '"></div>';
+        document.getElementById("myfriends").innerHTML += '<img style="width: 9%; padding: 1rem" src="img/' + v.img + '">';
         document.getElementById("myfriends").innerHTML
-        += '<div class="bfname"><p><b>[' + v.name + "]</b> " + v.detail + ' (' + v.group  + ')</p></div>'
+        += '<div class="bfname"><b>[' + v.name + "]</b> " + v.detail + ' (' + v.group  + ')</div><br>'
     });
 }
 
